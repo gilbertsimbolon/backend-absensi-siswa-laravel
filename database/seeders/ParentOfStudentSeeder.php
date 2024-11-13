50e8a45e346b247ede6a7dfbda3f6cc54d7e2676
gilbertsimbolon <gilbertsimbolon306@gmail.com>
2024-11-13 14:47:35 +0800

    make parentofstudent seeder

--- database/seeders/ParentOfStudentSeeder.php
+++ database/seeders/ParentOfStudentSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\ParentOfStudent;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
 
 class ParentOfStudentSeeder extends Seeder
 {
@@ -12,6 +14,23 @@ class ParentOfStudentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        ParentOfStudent::create([
+            'mother_name' => 'Morika Silitonga',
+            'father_name' => 'Matius Simbolon',
+            'kk_number' => '023103102930923',
+            'phone' => '082168693633',
+            'address' => 'Kota Medan',
+            'email' => 'silitongasimbolon@parents.com',
+            'password' => Hash::make('parent1'),
+        ]);
+        ParentOfStudent::create([
+            'mother_name' => 'Eka Manurung',
+            'father_name' => 'Simson Sianturi',
+            'kk_number' => '0231031321330923',
+            'phone' => '082168646333',
+            'address' => 'Doloksanggul',
+            'email' => 'manurungsianturi@parents.com',
+            'password' => Hash::make('parent2'),
+        ]);
     }
 }
--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -17,17 +17,43 @@ class StudentSeeder extends Seeder
     public function run(): void
     {
         Student::create([
-            'nisn',
-            'name',
-            'parent_id',
-            'phone',
-            'class_of_students_id',
-            'email',
-            'gender',
-            'place_born',
-            'date_born',
-            'address',
-            'foto',
+            'nisn' => '001',
+            'name' => 'Gilbert Simbolon',
+            'parent_id' => 1,
+            'phone' => '081367937852',
+            'class_of_students_id' => 1, 
+            'email' => 'gilbert@students.com',
+            'gender' => 'Pria',
+            'place_born' => 'Kota Medan',
+            'date_born' => '1 Juli 2005',
+            'address' => 'Jl.Sisingamangaraja',
+            'foto' => 'none',
+        ]);
+        Student::create([
+            'nisn' => '002',
+            'name' => 'Maria Simbolon',
+            'parent_id' => 1,
+            'phone' => '081367937852',
+            'class_of_students_id' => 2,
+            'email' => 'maria@students.com',
+            'gender' => 'Wanita',
+            'place_born' => 'Kota Medan',
+            'date_born' => '1 Juli 2006',
+            'address' => 'Jl.Sisingamangaraja',
+            'foto' => 'none',
+        ]);
+        Student::create([
+            'nisn' => '003',
+            'name' => 'Setyo Sianturi',
+            'parent_id' => 2,
+            'phone' => '081367937852',
+            'class_of_students_id' => 3,
+            'email' => 'setyo@students.com',
+            'gender' => 'Pria',
+            'place_born' => 'Doloksanggul',
+            'date_born' => '1 Juli 2006',
+            'address' => 'Lintong ni Huta',
+            'foto' => 'none',
         ]);
     }
 }
